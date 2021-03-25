1a4e8274e98bd3e90f0d6d3b8f95d2c10c356298
Jaime Salgado <jsalg10@gmail.com>
2021-03-25 14:05:59 -0500

    got client side files working with server side and responding with full object. Need to work with having input work with specific opjects

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = 8000
+
+app.use(cors())
+
+let PueblosMagicos = {
+    'Tulum': {
+    "Name": "Tulum",
+    "State": "Quintana Roo",
+    "Airport": "Cancun",
+    "image": "unsplash",
+    "wiki": "wiki goes here",
+    "wikiTravel": "https://en.wikivoyage.org/wiki/Tulum#Q725883"
+},
+'Isla Mujeres': {
+    "Name": "Isla Mujeres",
+    "State": "Quintana Roo",
+    "Airport": "Cancun",
+    "image": "unsplash",
+    "wiki": "wiki goes here",
+    "wikiTravel": "https://en.wikivoyage.org/wiki/Isla_Mujeres#Q1020769"
+},
+'Unkown': {
+    "Name": "No se",
+    "State": "No se",
+    "Airport": "No se",
+    "image": "No se",
+    "wiki": "No se",
+    "wikiTravel": "No se"
+}
+}
 
 app.get('/',(request, response) => {
     response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/PueblosMagicos', (request, response)=>{
+    response.json(PueblosMagicos)
 })
 
 app.listen(PORT, ()=>{
