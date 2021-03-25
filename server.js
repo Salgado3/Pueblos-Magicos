15f3075faa776bf9c9d9a8cc984b134da4560f51
Jaime Salgado <jsalg10@gmail.com>
2021-03-25 10:43:11 -0500

    building true server.js

--- server.js
+++ server.js
@@ -1,68 +1,12 @@
-// noting our expess and housing it in variable app
-const { response } = require('express')
 const express = require('express')
 const app = express()
-//allows to add new notes
-app.use(express.json())
+const PORT = 8000
 
-//this will be our API object with a list of pueblos magicos
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2019-05-30T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2019-05-30T18:39:34.091Z",
-        important: false
-    },
-    {   id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2019-05-30T19:20:14.298Z",
-        important: true
-    }
-]
-//requesting the home page from the server
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
-  
+app.get('/',(request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
 
-  //requesting json file from the server
-  app.get('/api/notes', (request, response) => {
-    response.json(notes)
-  })
-//fetching specific items by id
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log(id)
-    const note = notes.find(note => note.id === id)
+app.listen(PORT, ()=>{
+    console.log(`Server running on ${PORT}`)
+})
 
-    if (note){    
-        response.json(note)  
-    }else {response.status(404).end()  
-
-    }
-  })
-
-  //deleting resources
-  app.delete('/api/notes/:id', (request, response) => {
-      const id = Number(request.params.id)
-      notes = notes.filter(note => note.id !== id)
-
-      response.status(204).end()
-  })
-
-  //allows us to add resources
-  app.post('/api/notes', (request, response) => {
-    const note = request.body
-    console.log(note)
-    response.json(note)
-    })
-
-const PORT = 3001 
-app.listen(PORT)
-console.log(`Server is running on port ${PORT}`)
